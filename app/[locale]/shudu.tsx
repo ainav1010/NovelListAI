ac9978f71b5fb6b16238cc1de60a3b80d7208954
ainav1010 <ainav@iaiuse.com>
2024-12-09 14:22:59 +0800

    Create shudu.tsx

---
+++ app/[locale]/shudu.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react';
+import EmojiSudoku from '@/components/Games/Shudu';
+
+const Index: React.FC = () => {
+  
+  return (
+    <div className="min-h-screen bg-purple-50">
+      <EmojiSudoku />
+    </div>
+  );
+};
+
+export default Index;
